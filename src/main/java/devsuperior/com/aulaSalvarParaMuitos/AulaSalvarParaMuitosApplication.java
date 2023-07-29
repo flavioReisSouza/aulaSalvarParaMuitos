53f0dec180cda5b6827870dba08392e6ee131f4e
Flavio Reis <flavioreissouza@yahoo.com>
2023-07-29 15:44:32 -0300

    Creating packages

---
+++ src/main/java/devsuperior/com/aulaSalvarParaMuitos/AulaSalvarParaMuitosApplication.java
@@ -0,0 +1,13 @@
+package devsuperior.com.aulaSalvarParaMuitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSalvarParaMuitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSalvarParaMuitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devsuperior/com/aulaSalvarParaMuitos/AulaSalvarParaMuitosApplicationTests.java
@@ -0,0 +1,13 @@
+package devsuperior.com.aulaSalvarParaMuitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSalvarParaMuitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
